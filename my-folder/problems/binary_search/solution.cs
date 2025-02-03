66be848447072293d5f7ae7c53ef53f9028deda0
Riyan <74947287+Ri-yan@users.noreply.github.com>
2025-02-03 04:23:39 +0000

    Sync LeetCode submission - Binary Search (csharp)

--- my-folder/problems/binary_search/solution.cs
+++ my-folder/problems/binary_search/solution.cs
@@ -1,24 +1,13 @@
 public class Solution {
+    public static int recSearch(int[] nums, int low,int high,int target){
+        if(low>high)return -1;
+        int mid=(low+high)/2;
+        if(target==nums[mid]) return mid;
+
+        if(nums[mid]>target) return recSearch(nums,low,mid-1,target);
+        else  return recSearch(nums,mid+1,high,target);
+    }
     public int Search(int[] nums, int target) {
-        
-        int low=0;
-        int high=nums.Length-1;
-        while (low <= high) {
-        int mid = low + (high - low) / 2;
-
-        // Check if x is present at mid
-        if (nums[mid] == target)
-            return mid;
-
-        // If x greater, ignore left half
-        if (nums[mid] < target)
-            low = mid + 1;
-
-        // If x is smaller, ignore right half
-        else
-            high = mid - 1;
-         
-        }
-        return -1;
+        return recSearch(nums, 0,nums.Length-1,target);
     }
 }
