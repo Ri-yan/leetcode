79a72f25b602cbb5f8cbb444ca3de5d3ce71a325
Riyan <74947287+Ri-yan@users.noreply.github.com>
2025-02-11 16:49:11 +0000

    Sync LeetCode submission - Search Insert Position (csharp)

--- my-folder/problems/search_insert_position/solution.cs
+++ my-folder/problems/search_insert_position/solution.cs
@@ -1,27 +1,17 @@
 public class Solution {
-    public static int binarySearch(int[] nums,int low,int high, int target){
-        if(low>high) return target;
-        int mid = (low+high)/2;
-        if(target==nums[mid]) return mid;
-        if(target<=nums[mid]){
-            return binarySearch(nums,low,mid-1, target);
-        }else{
-             return binarySearch(nums,mid+1,high, target);
-        }
-    }
     public int SearchInsert(int[] nums, int target) {
-       int low=0;
-       int high=nums.Length-1;
-       int ans=nums.Length;
-       while(low<=high){
-        int mid=(low+high)/2;
-        if(nums[mid]>=target){
-            ans = mid;
-            high = mid-1;
+      int l =0;
+      int h=nums.Length-1;  
+      int ans=nums.Length;
+      while(l<=h){
+        int m = (l+h)/2;
+        if(nums[m]>=target){
+            ans=m;
+            h=m-1;
         }else{
-            low = mid+1;
+            l=m+1;
         }
-       }
+      }
       return ans;
     }
 }
