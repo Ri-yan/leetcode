cbc0c46bd225a778cb3dd255b0a296716c80c159
Riyan <74947287+Ri-yan@users.noreply.github.com>
2025-04-14 04:27:55 +0000

    Sync LeetCode submission - Binary Tree Inorder Traversal (csharp)

---
+++ my-folder/problems/binary_tree_inorder_traversal/solution.cs
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    static void Traversal(TreeNode root,IList<int> res) {
+        if(root==null){
+            return ;
+        }
+        Traversal(root.left, res);
+        res.Add(root.val);
+        Traversal(root.right, res);
+    }
+    public IList<int> InorderTraversal(TreeNode root) {
+        IList<int> res =  new List<int>();
+        Traversal(root, res);
+        return res;
+    }
+}
