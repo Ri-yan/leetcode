74b7e397041d193122951c6a9249ed1185c3f734
Riyan <74947287+Ri-yan@users.noreply.github.com>
2025-05-25 11:33:11 +0000

    Sync LeetCode submission - Remove Duplicates from Sorted Array (csharp)

--- my-folder/problems/remove_duplicates_from_sorted_array/solution.cs
+++ my-folder/problems/remove_duplicates_from_sorted_array/solution.cs
@@ -1,6 +1,6 @@
 public class Solution {
     public int RemoveDuplicates(int[] nums) {
-       /*HashSet<int> s =new HashSet<int>();
+       HashSet<int> s =new HashSet<int>();
        for(int i=0;i<nums.Length;i++){
         s.Add(nums[i]);
        }
@@ -9,14 +9,14 @@ public class Solution {
             nums[u]= num;
             u++;
        }
-       return u;*/
-       int u=0;
-       for(int i=0;i<nums.Length;i++){
-        if(nums[u]!=nums[i]){
+       return u;
+    //    int u=0;
+    //    for(int i=0;i<nums.Length;i++){
+    //     if(nums[u]!=nums[i]){
            
-            nums[u+1]=nums[i];   u++;
-        }
-       }
-       return u+1;
+    //         nums[u+1]=nums[i];   u++;
+    //     }
+    //    }
+    //    return u+1;
     }
 }
