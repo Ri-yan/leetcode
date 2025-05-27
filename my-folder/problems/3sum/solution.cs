895989526e2433ea7aae6f787d4762dc1f421546
Riyan <74947287+Ri-yan@users.noreply.github.com>
2025-05-27 14:32:10 +0000

    Sync LeetCode submission - 3Sum (csharp)

--- my-folder/problems/3sum/solution.cs
+++ my-folder/problems/3sum/solution.cs
@@ -1,53 +1,30 @@
 public class Solution {
     public IList<IList<int>> ThreeSum(int[] nums) {
-          IList<IList<int>> result = new List<IList<int>>();
-    int n = nums.Length;
-
-    // for (int i = 0; i < n ; i++) {
-    //     int sum=nums[i];
-    //     for (int j = i; j < n ; j++) {
-    //          int tsum=0;
-    //         if(j>i)
-    //         tsum=nums[j]+nums[i];
-    //         for (int k = j; k < n; k++) {
-    //             if(nums[k]+tsum==0 && i!=j &&j!=k && k>j) {
-    //             var triplet = new List<int> { nums[i], nums[j], nums[k] };
-    //             triplet.Sort(); 
-    //             result.Add(triplet);
-    //             }
-                
-    //         }
-    //     }
-    // }
-   
-    // foreach (var triplet in distinctLists) {
-    //     Console.WriteLine($"[{string.Join(", ", triplet)}]");
-    // }
-
-    // return result; 
-
+        IList<IList<int>> res = new List<IList<int>>();
         Array.Sort(nums);
-        for(int i=0;i<n;i++){
-           int j=i+1;
-           int k=n-1;
-           if (i != 0 && nums[i] == nums[i - 1]) continue;
-           while(j<k) {
-            int t=nums[i]+nums[j]+nums[k];
-            if(t<0){
-                j++;
-            } else if(t>0){
-                k--;
-            }else{
-              result.Add(new List<int>{nums[i],nums[j],nums[k]});
-              j++;
-              k--;
-                while(j<k && nums[j]==nums[j-1]){ j++;}
-                while(j<k && nums[k]==nums[k+1]){ k--;}
+        int l= nums.Length;
+        for(int i=0;i<l;i++){
+            int j = i+1;
+            int k=l-1;
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+          
+            while(j<k){
+                var r = nums[i]+nums[j]+nums[k];
+               if(r>0){
+                    k--;
+                }else if(r<0){
+                    j++;
+                }else{
+                  res.Add(new List<int>{nums[i],nums[j],nums[k]});
+                  j++;
+                  k--;
+                     while(j<k && nums[j]==nums[j-1]){ j++;}
+                     while(j<k && nums[k]==nums[k+1]){ k--;}
+                }
             }
-           
-           }
-        }
-
-        return result;
+            }
+        return res;
     }
 }
+
+//[-1,0,1,2,-1,-4]
