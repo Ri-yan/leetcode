eafe7afe2b02eef672e47d9b9d26d60ea1586eb0
Riyan <74947287+Ri-yan@users.noreply.github.com>
2025-06-04 08:26:52 +0000

    Sync LeetCode submission - Find First and Last Position of Element in Sorted Array (csharp)

--- my-folder/problems/find_first_and_last_position_of_element_in_sorted_array/solution.cs
+++ my-folder/problems/find_first_and_last_position_of_element_in_sorted_array/solution.cs
@@ -1,6 +1,39 @@
 public class Solution {
+    int FirstOccurence(int[] nums, int target ){
+        int s=0;
+        int e=nums.Length-1;
+        int f=-1;
+        while(s<=e){
+            int m = (s+e)/2;
+            if(nums[m]==target){
+                f=m;
+                e=m-1;
+            }else if(nums[m]<target) s=m+1;
+            else  e=m-1;
+        }
+        return f;
+    }
+    int LastOccurence(int[] nums, int target ){
+        int s=0;
+        int e=nums.Length-1;
+        int l=-1;
+        while(s<=e){
+            int m = (s+e)/2;
+            if(nums[m]==target){
+                l=m;
+                s=m+1;
+            }else if(nums[m]<target) s=m+1;
+            else e=m-1;
+        }
+
+        return l;
+    }
     public int[] SearchRange(int[] nums, int target) {
         int f=-1,l=-1;
+        f=FirstOccurence(nums,target );
+        if(f==-1)return new int[]{f,l};
+        l=LastOccurence(nums,target );
+        return new int[]{f,l};
         for(int i=0;i<nums.Length;i++){
             if(nums[i]==target){
                 if(f==-1){
@@ -9,23 +42,6 @@ public class Solution {
                 l=i;
             }
         }
-
-        // int s=0;
-        // int e=nums.Length-1;
-        // while(s<=e){
-        //     int m = (s+e)/2;
-        //     if(nums[m]==target){
-        //         if(f==-1){
-        //             f=m;
-        //         }
-        //         l=m;
-        //     }
-        //     if(nums[m]<target){
-        //         s=m+1;
-        //     }else{
-        //         e=m-1;
-        //     } 
-        // }
         return new int[]{f,l};
     }
 }
