2cdab5b4eb3f8ba75fc41af0572b05604b4a2248
Riyan <74947287+Ri-yan@users.noreply.github.com>
2025-07-06 12:20:58 +0000

    Sync LeetCode submission - Longest Common Prefix (csharp)

--- my-folder/problems/longest_common_prefix/solution.cs
+++ my-folder/problems/longest_common_prefix/solution.cs
@@ -1,25 +1,23 @@
 public class Solution {
     public string LongestCommonPrefix(string[] strs) {
-         if (strs == null || strs.Length == 0) return "";
-        string result="";
-        int min = strs[0].Length;
+        if (strs == null || strs.Length == 0) return "";
 
-        for(int i=0;i<strs.Length;i++){
-            min = Math.Min(min,strs[i].Length);
-        }
-        
-        for(int c=min;c>=0;c--){
-            string d = strs[0].Substring(0,c); 
-            int t=0;
-            for(int i=0;i<strs.Length;i++){
-                if(d==strs[i].Substring(0,c)){
-                
-                if(t==strs.Length-1) return d;t++;
-                }else{
-                result= "";
+        int min = strs[0].Length;
+        foreach (var s in strs)
+            min = Math.Min(min, s.Length);
+
+        for (int c = min; c >= 0; c--) {
+            string prefix = strs[0].Substring(0, c);
+            bool isCommon = true;
+            for (int i = 1; i < strs.Length; i++) {
+                if (!strs[i].StartsWith(prefix)) {
+                    isCommon = false;
+                    break;
                 }
             }
+            if (isCommon) return prefix;
         }
-        return result;
+
+        return "";
     }
 }
