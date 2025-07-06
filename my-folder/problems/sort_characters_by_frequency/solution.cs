969f3f0e7f581074a0c6ba2cec7177ee46084563
Riyan <74947287+Ri-yan@users.noreply.github.com>
2025-07-06 13:33:48 +0000

    Sync LeetCode submission - Sort Characters By Frequency (csharp)

--- my-folder/problems/sort_characters_by_frequency/solution.cs
+++ my-folder/problems/sort_characters_by_frequency/solution.cs
@@ -1,36 +1,23 @@
-using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Linq;
-
 public class Solution {
     public string FrequencySort(string s) {
-        // Create a dictionary to store character frequencies
-        Dictionary<char, int> cnt = new Dictionary<char, int>();
-
-        // Count the frequency of each character
-        for (int i = 0; i < s.Length; i++) {
-            if (cnt.ContainsKey(s[i])) {
-                cnt[s[i]]++;
-            } else {
-                cnt[s[i]] = 1;
+        // Step 1: Count frequencies
+        Dictionary<char, int> freq = new Dictionary<char, int>();
+        foreach (char c in s) {
+            if (!freq.ContainsKey(c)) {
+                freq[c] = 0;
             }
+            freq[c]++;
         }
 
-        // Create a list of characters from the dictionary keys
-        List<char> cs = cnt.Keys.ToList();
-        
-        // Sort the characters by frequency in descending order
-        cs.Sort((a, b) => cnt[b] - cnt[a]);
+        // Step 2: Sort characters by frequency in descending order
+        var sorted = freq.OrderByDescending(kvp => kvp.Value);
 
-        // Build the result string
-        StringBuilder ans = new StringBuilder();
-        foreach (char c in cs) {
-            for (int v = cnt[c]; v > 0; v--) {
-                ans.Append(c);
-            }
+        // Step 3: Build result string
+        StringBuilder sb = new StringBuilder();
+        foreach (var kvp in sorted) {
+            sb.Append(new string(kvp.Key, kvp.Value));
         }
 
-        return ans.ToString();
+        return sb.ToString();
     }
 }
